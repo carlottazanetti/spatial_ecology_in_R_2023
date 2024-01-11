2ef10cf5ee2f0aaf4bd0cfd39414f0f6f62814e8
carlottazanetti <128401963+carlottazanetti@users.noreply.github.com>
2024-01-11 17:30:45 +0100

    Create colorblind.R

---
+++ colorblind.R
@@ -0,0 +1,12 @@
+%simulating colorblindess
+
+library(devtools)
+library(colorblindr)
+
+install_github("clauswilke/colorblindr")
+
+library(ggplot2)
+fig <- ggplot(iris, aes(Sepal.Length, fill = Species)) + geom_density(alpha = 0.7)
+fig
+
+cvd_grid(fig)
